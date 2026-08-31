adaa3c5ebcce06607240113ac574253be20a7934
Kelly Allison Rademeyer <u25571878@tuks.co.za>
2026-08-31 13:02:36 +0200

    Add files via upload

--- FoodVendor.cpp
+++ FoodVendor.cpp
@@ -1,3 +1,30 @@
 #include "FoodVendor.h"
 #include <iostream>
 
+FoodVendor::FoodVendor(const std::string& nameIn): EventUnit(nameIn, 40) {}
+
+void FoodVendor::update(const Notice& notice) {
+    switch (notice.type) {
+        case NoticeType::OPEN:
+            open();
+            break;
+        case NoticeType::CLOSE:
+        case NoticeType::EVACUATE:
+            close();
+            std::cout << "[food] " << name << " secures cooking equipment and stops service.\n";
+            break;
+        case NoticeType::WEATHER_ALERT:
+            std::cout << "[food] " << name << " secures outdoor equipment and continues only if safe.\n";
+            break;
+        case NoticeType::CAPACITY_ALERT:
+        case NoticeType::CROWD_SURGE:
+            std::cout << "[food] " << name << " keeps queues inside marked vendor lanes.\n";
+            break;
+        case NoticeType::HYDRATION_ALERT:
+            std::cout << "[food] " << name<< " highlights high-water food options.\n";
+            break;
+        default:
+            std::cout << "[food] " << name << " records notice: " << notice.message << "\n";
+            break;
+    }
+}
--- FoodVendor.h
+++ FoodVendor.h
@@ -1,7 +1,17 @@
+/*u25571878: Kelly Rademeyer
+u25044215: Hannah diedrick
+u25038967: Shelby bodenstein
+*/
 #ifndef FOOD_VENDOR_H
 #define FOOD_VENDOR_H
 
 #include "EventUnit.h"
 
+class FoodVendor : public EventUnit {
+public:
+    explicit FoodVendor(const std::string& name);
+    virtual ~FoodVendor() {}
+    virtual void update(const Notice& notice);
+};
 
 #endif
