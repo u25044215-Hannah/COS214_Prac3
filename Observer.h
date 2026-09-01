e0422e89fb951b3a13cc7ecda53e1c2fe1a262b3
u25044215-Hannah <u25044215@tuks.co.za>
2026-09-01 05:47:16 +0200

    Doxyfile class code

--- Observer.h
+++ Observer.h
@@ -2,17 +2,26 @@
 u25044215: Hannah diedrick
 u25038967: Shelby bodenstein
 */
-#ifndef OBSERVER_H
-#define OBSERVER_H
-
-#include "Notice.h"
-
-
+/**
+ * @brief Abstract Observer participant in the Lentedag notification system.
+ *
+ * Observer defines the interface that all objects interested in festival
+ * notices must implement. Subjects notify registered observers by calling
+ * update() and pushing a Notice object to them.
+ */
 class Observer {
 public:
+
+    /**
+     * @brief Virtual destructor for safe polymorphic destruction.
+     */
     virtual ~Observer() {}
 
+    /**
+     * @brief Receive a notice pushed by a Subject.
+     *
+     * Concrete observers implement this operation to perform behaviour appropriate to the received notice.
+     * @param notice Festival notice containing the type, message, source, location and optional numeric value.
+     */
     virtual void update(const Notice& notice) = 0;
 };
-
-#endif
