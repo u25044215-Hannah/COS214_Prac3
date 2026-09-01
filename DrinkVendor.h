1035be9ebc502a13232c366fdbcd08412bf8d527
u25044215-Hannah <u25044215@tuks.co.za>
2026-09-01 06:00:18 +0200

    Enhance DrinkVendor class with documentation comments
    Added documentation comments for the DrinkVendor class and its methods.

--- DrinkVendor.h
+++ DrinkVendor.h
@@ -2,20 +2,36 @@
 u25044215: Hannah diedrick
 u25038967: Shelby bodenstein
 */
-#ifndef DRINK_VENDOR_H
-#define DRINK_VENDOR_H
+/**
+ * @brief Concrete Leaf representing a Lentedag beverage vendor or water point.
 
-#include "EventUnit.h"
-
-
+ */
 class DrinkVendor : public EventUnit {
 public:
+
+    /**
+     * @brief Construct a drink vendor.
+     *
+     * @param name Human-readable vendor name.
+     */
     explicit DrinkVendor(const std::string& name);
+
+    /**
+     * @brief Virtual destructor.
+     */
     virtual ~DrinkVendor() {}
+
+    /**
+     * @brief Respond to a festival Notice.
+     *
+     * @param notice Notice pushed to this drink vendor.
+     */
     virtual void update(const Notice& notice);
 
 private:
+
+    /**
+     * @brief Indicates whether the vendor is currently prioritising water.
+     */
     bool waterPriority;
 };
-
-#endif
