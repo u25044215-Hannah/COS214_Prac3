2cc2732391782e37744985360e399256c4da7767
u25044215-Hannah <u25044215@tuks.co.za>
2026-09-01 05:59:41 +0200

    Enhance FoodVendor class with documentation comments
    Added documentation comments for the FoodVendor class and its methods.

--- FoodVendor.h
+++ FoodVendor.h
@@ -2,16 +2,29 @@
 u25044215: Hannah diedrick
 u25038967: Shelby bodenstein
 */
-#ifndef FOOD_VENDOR_H
-#define FOOD_VENDOR_H
+/**
+ * @brief Concrete Leaf representing a Lentedag food vendor.
 
-#include "EventUnit.h"
-
+ */
 class FoodVendor : public EventUnit {
 public:
+
+    /**
+     * @brief Construct a food vendor.
+     *
+     * @param name Human-readable vendor name.
+     */
     explicit FoodVendor(const std::string& name);
+
+    /**
+     * @brief Virtual destructor.
+     */
     virtual ~FoodVendor() {}
+
+    /**
+     * @brief Respond to a festival Notice.
+     *
+     * @param notice Notice pushed to the vendor.
+     */
     virtual void update(const Notice& notice);
 };
-
-#endif
