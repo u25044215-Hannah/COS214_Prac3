214691e74217b77ca4efa920d572a09678ca16e3
u25044215-Hannah <u25044215@tuks.co.za>
2026-09-01 06:02:43 +0200

    Refactor InfoPoint class with documentation and cleanup

--- InfoPoint.h
+++ InfoPoint.h
@@ -2,20 +2,40 @@
 u25044215: Hannah diedrick
 u25038967: Shelby bodenstein
 */
-#ifndef INFO_POINT_H
-#define INFO_POINT_H
-
-#include "EventUnit.h"
-
+/**
+ * @brief Concrete Leaf representing a Lentedag information or display point.
+ */
 class InfoPoint : public EventUnit {
 public:
+
+    /**
+     * @brief Construct an information point.
+     *
+     * @param name Human-readable name of the information point.
+     */
     explicit InfoPoint(const std::string& name);
+
+    /**
+     * @brief Virtual destructor.
+     */
     virtual ~InfoPoint() {}
+
+    /**
+     * @brief Update the information display using the received Notice.
+     *
+     * @param notice Notice containing the message to display.
+     */
     virtual void update(const Notice& notice);
+
+    /**
+     * @brief Report the current information-point status and display message.
+     */
     virtual void reportStatus() const;
 
 private:
+
+    /**
+     * @brief Message currently displayed to festival attendees.
+     */
     std::string displayMessage;
 };
-
-#endif
